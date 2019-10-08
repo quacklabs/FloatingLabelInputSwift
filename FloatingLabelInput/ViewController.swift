e7514282a905d945553e05a8ee683ffb4b02a66e
Mark Boleigha <donhoenix@gmail.com>
2019-10-08 15:40:51 +0100

    Initial Commit

---
+++ FloatingLabelInput/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FloatingLabelInput
+//
+//  Created by Sprinthub on 08/10/2019.
+//  Copyright © 2019 Sprinthub Mobile. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

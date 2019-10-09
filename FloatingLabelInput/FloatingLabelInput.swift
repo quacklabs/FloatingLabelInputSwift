e1b5092ab12a4391556478eb765d8de1bf9ebb12
Mark Boleigha <donhoenix@gmail.com>
2019-10-09 13:34:13 +0100

    initial commit

---
+++ FloatingLabelInput/FloatingLabelInput.swift
@@ -0,0 +1,9 @@
+//
+//  FloatingLabelInput.swift
+//  FloatingLabelInput
+//
+//  Created by Sprinthub on 09/10/2019.
+//  Copyright © 2019 Sprinthub Mobile. All rights reserved.
+//
+
+import Foundation
